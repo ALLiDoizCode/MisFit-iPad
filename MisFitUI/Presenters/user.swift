fae736a9e0a1c3baef7b8d4a54b5131b352d330b
Jonathan Green <jonathangreen@Jonathans-iMac-2.local>
2015-12-07 21:25:30 -0500

    WIP added func for username

---
+++ MisFitUI/Presenters/user.swift
@@ -0,0 +1,20 @@
+//
+//  user.swift
+//  MisFitiPad
+//
+//  Created by Jonathan Green on 12/7/15.
+//  Copyright © 2015 Jonathan Green. All rights reserved.
+//
+
+import Foundation
+
+class user {
+    
+    func userName() ->String {
+        
+        let name = "christ"
+        
+        return name
+        
+    }
+}
